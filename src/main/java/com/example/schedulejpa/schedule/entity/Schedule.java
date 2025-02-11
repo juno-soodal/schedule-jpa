dc4d485fe667d2ae9a0f0a72a2f3e6bae5598686
jun5-soodal <j01024749503@gmail.com>
2025-02-11 20:08:53 +0900

    refactor(schedule): change authorName field to memberId add ManyToOne relation

--- src/main/java/com/example/schedulejpa/schedule/entity/Schedule.java
+++ src/main/java/com/example/schedulejpa/schedule/entity/Schedule.java
@@ -7,6 +7,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -23,6 +24,7 @@ public class Schedule extends BaseEntity {
 
     //    private String authorName;
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
     private Member member;
 
     private String title;
