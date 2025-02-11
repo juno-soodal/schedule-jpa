d8bad5427e3c04f0a183a7144d2f40a9c85aa96e
jun5-soodal <j01024749503@gmail.com>
2025-02-11 13:18:23 +0900

    feat(schedule):add Schedule entity

---
+++ src/main/java/com/example/schedulejpa/schedule/entity/Schedule.java
@@ -0,0 +1,27 @@
+package com.example.schedulejpa.schedule.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "schedule")
+public class Schedule extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String authorName;
+
+    private String title;
+
+    @Lob
+    private String content;
+
+}
