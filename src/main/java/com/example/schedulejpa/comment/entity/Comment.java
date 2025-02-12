a1b348076b55a7a86fcb7d288f3438db80c44479
jun5-soodal <j01024749503@gmail.com>
2025-02-12 17:43:26 +0900

    feat(comment):add Comment entity

---
+++ src/main/java/com/example/schedulejpa/comment/entity/Comment.java
@@ -0,0 +1,38 @@
+package com.example.schedulejpa.comment.entity;
+
+import com.example.schedulejpa.global.entity.BaseEntity;
+import com.example.schedulejpa.member.entity.Member;
+import com.example.schedulejpa.schedule.entity.Schedule;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@SQLRestriction("deleted_at is null")
+@Table(name = "comment")
+public class Comment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String commentContent;
+
+    private LocalDateTime deletedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
