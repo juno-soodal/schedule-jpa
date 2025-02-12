8eb50cbd00d10a457f6aab95b3ec6a87ab60f876
jun5-soodal <j01024749503@gmail.com>
2025-02-13 00:17:04 +0900

    chore:test 파일 삭제

--- src/main/java/com/example/schedulejpa/schedule/repository/Test.java
+++
@@ -1,9 +0,0 @@
-package com.example.schedulejpa.schedule.repository;
-
-import com.example.schedulejpa.schedule.entity.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Test extends JpaRepository<Schedule, Long> {
-
-
-}
