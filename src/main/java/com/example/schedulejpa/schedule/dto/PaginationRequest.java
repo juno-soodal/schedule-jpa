729904e1f58cedf49b9876de5ee701c83aceea9c
jun5-soodal <j01024749503@gmail.com>
2025-02-13 16:39:49 +0900

    refactor(schedule):페이지네이션 구조 변경

--- src/main/java/com/example/schedulejpa/schedule/dto/PaginationRequest.java
+++ src/main/java/com/example/schedulejpa/schedule/dto/PaginationRequest.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 public class PaginationRequest {
 
-    @Min(value = 0, message = "페이지는 0번부터 시작입니다.")
+    @Min(value = 1, message = "페이지는 1번부터 시작입니다.")
     private int page;
     @Min(value = 10, message = "사이즈는 10부터 시작입니다.")
     private int size;
